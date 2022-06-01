ef99c448ae329b3f8abd54cbfcea4efb402e33b8
Reberti Libba Danilo <>
2022-06-01 12:57:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print('----------')
+print('Cadastro')
+print('----------')
+
+nome= input ('Qual seu nome ?' )
+idade= input('Qual é a sua idade ?' )
+peso= input('Qual é o seu peso ?' )
+print(nome,idade, ('anos'),peso, ('kg'))
